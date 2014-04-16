95cbcfcda20fe245d5882d8acc1baf52dcc3da67
TomCroft <tomrcroft@yahoo.com>
2014-04-16 00:10:05 -0700

    Some Database Functions Implemented
    Functions:
    changePassword
    validatePassword
    changeEmail
    changeJobDescription
    changeSkypeID

--- DatabaseFunctions.php
+++ DatabaseFunctions.php
@@ -1,16 +1,49 @@
 <?php
 
+//connect to DB
+$con=mysqli_connect("mydbinstance.czp48rfeukis.us-west-2.rds.amazonaws.com","tomrcroft","password","cmpe133Project");
+
 //change password function
+function changePassword($uName, $password)
+{
+    global $con;
+    mysqli_query($con,"update userInfo set password='$password' where username='$uName'");
+}
 
 //validate password function
+function validatePassword($uName, $password)
+{
+    global $con;
+    $result = mysqli_query($con,"select password from userInfo where username='Tom'");
+    $resultArray = mysqli_fetch_array($result);
+    if($resultArray[0] == $password)
+        return "true";
+    else
+        return "false";
+}
 
 //change email function
+function changeEmail($uName, $email)
+{
+    global $con;
+    mysqli_query($con,"UPDATE userInfo SET email='$email' WHERE username='$uName'");
+}
 
 //get interests function
 
 //set job description function
+function changeJobDescription($uName, $jobDescrib)
+{
+    global $con;
+    mysqli_query($con,"UPDATE userInfo SET jobDescription='$jobDescrib' WHERE username='$uName'");
+}
 
 //change skypeID function
+function changeSkypeID($uName, $ID)
+{
+    global $con;
+    mysqli_query($con,"UPDATE userInfo SET skypeID='$ID' WHERE username='$uName'");
+}
 
 //get mentees function
 
