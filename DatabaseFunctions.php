480f7898109b9632df39ef2370cbe506a9c98423
TomCroft <tomrcroft@yahoo.com>
2014-04-21 00:32:16 -0700

    Database Function Update 2
    functions to return information

--- DatabaseFunctions.php
+++ DatabaseFunctions.php
@@ -1,16 +1,41 @@
 <?php
+/**
+  * This file contains all the methods that interact with the Database.
+  *
+  * This file represents the gateway to the database. It should be considered a library of database access. 
+  * The purpose of this file is so that database access can be controlled and manipulated in one class 
+  * instead of functions being scattered throughout the application
+  *
+  */
 
 //connect to DB
 $con=mysqli_connect("mydbinstance.czp48rfeukis.us-west-2.rds.amazonaws.com","tomrcroft","password","cmpe133Project");
 
-//change password function
+/**
+  * Function that will be used to change a person's password.
+  *
+  * @param String $uName is the username of the user whos password will be changed.
+  * @param String $password is the replacement password that will be set into the database.
+  *
+  * @return void
+  */
 function changePassword($uName, $password)
 {
     global $con;
     mysqli_query($con,"update userInfo set password='$password' where username='$uName'");
 }
 
-//validate password function
+/**
+  * A summary informing the user what the associated element does.
+  *
+  * A *description*, that can span multiple lines, to go _in-depth_ into the details of this element
+  * and to provide some background information or textual references.
+  *
+  * @param String $uName is the username of the user whos password will be changed.
+  * @param 
+  *
+  * @return will be "true" if the password matches that username.
+  */
 function validatePassword($uName, $password)
 {
     global $con;
@@ -43,6 +68,51 @@ function changeSkypeID($uName, $ID)
 {
     global $con;
     mysqli_query($con,"UPDATE userInfo SET skypeID='$ID' WHERE username='$uName'");
+}
+
+function getEmail($uName)
+{
+    global $con;
+    
+    $result = mysqli_query($con,"select email from userInfo WHERE username='$uName'");
+    $resultArray = mysqli_fetch_array($result);
+    return $resultArray[0];
+}
+
+function getPassword($uName)
+{
+    global $con;
+    
+    $result = mysqli_query($con,"select password from userInfo WHERE username='$uName'");
+    $resultArray = mysqli_fetch_array($result);
+    return $resultArray[0];
+}
+
+function getjobDescription($uName)
+{
+    global $con;
+    
+    $result = mysqli_query($con,"select jobDescription from userInfo WHERE username='$uName'");
+    $resultArray = mysqli_fetch_array($result);
+    return $resultArray[0];
+}
+
+function getskypeID($uName)
+{
+    global $con;
+    
+    $result = mysqli_query($con,"select skypeID from userInfo WHERE username='$uName'");
+    $resultArray = mysqli_fetch_array($result);
+    return $resultArray[0];
+}
+
+function getInterests($uName)
+{
+    global $con;
+    
+    $result = mysqli_query($con,"select * from interests WHERE username='$uName'");
+    $resultArray = mysqli_fetch_array($result);
+    return $resultArray;
 }
 
 //get mentees function
