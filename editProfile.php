184859ff6edcc122d63cf389b091d96029b0d127
mmilanovich <mmilanovich@gmail.com>
2014-04-21 21:51:54 -0700

    Deleted index.php, edits to my pages
    Added in some comments, and cleaned up some stuff in editProfile.php
    and viewProfiles.php.

--- editProfile.php
+++ editProfile.php
@@ -1,18 +1,59 @@
-<?php session_start(); ?>
+<?php 
+/*
+ * Created on Apr 10, 2014
+ *
+ * This is the main page for a user to view and edit their profile information 
+ * 
+ * It interfaces with the database backend through the DatabaseFunction.php script file.
+ * It includes methods for a user to change their password, email, interests, 
+ * job description, and skype ID. It also allows for a user to delete a mentor or mentee
+ * relationship.
+ */
+// initialize the session and grant access to the database functions
+session_start(); 
+include 'DatabaseFunctions.php';
+?>
 <html>
 <head>
 </head>
 <body>
 <h1>Edit Profile</h1>
 <br>
-<form method="post" action="changePassword.php">
-Old Password: <input type="password" name="oldpassword"><br>
-New Password: <input type="password" name="newpassword"><br>
-Repeat New Password: <input type="password" name="repeatnewpassword"><br>
-<input type="submit" value="Submit" name="passwordsubmit">
+
+<!-- Form for changing a users password, makes a self reference call for security -->
+<form method="post" action="editProfile.php">
+Old Password: <input type="password" name="oldpassword">
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['passwordsubmit'])) {
+		//if (!validatePassword($_SESSION['myusername'], $_POST['oldpassword'])) {
+    		echo '<p>Incorrect Password, please try again</p>';
+    		$correctPassword = false;
+    	//}
+    	//else 
+    		//echo '<br>';
+	
+} else {
+	echo '<br>';
+}
+echo 'New Password: <input type="password" name="newpassword"><br>';
+echo 'Repeat New Password: <input type="password" name="repeatnewpassword">';
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['passwordsubmit']) && $correctPassword) {
+	if ($_POST['newpassword'] == $_POST['repeatnewpassword']) {	
+    	echo '<p>Password submitted</p>';
+        //changePassword($_SESSION['myusername'], $_POST['newpassword']);
+   	} else {
+        echo '<p>New passwords do not match, please try again</p>';
+    }
+    echo '<br>';
+    
+}
+echo '<input type="submit" value="Submit" name="passwordsubmit">';
+?>
 </form>
 
 <?php
+// Check if the user is paid, if not, add a link to become a paid member, otherwise, 
+// show button to change a user's credit card information.
 $paid = false; //remove this line, change next to if ($_SESSION['paid'] == false)
 if (!$paid) {
     echo '<p>Would you like to become a premium member and remove all limits on mentors and mentees?</p>';
@@ -23,18 +64,26 @@ if (!$paid) {
     echo '<input type="button" onlick="changeCreditCard.php" value="Change">';
 }
 ?>
-
+<!-- Form to change a user's email-->
 <p>Email: 
 <?php 
-$email = 'someemail@email.com';
-echo $email;
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['emailsubmit'])) {
+	//changeEmail($_SESSION['username'], $_POST['email']);  
+	//$email = $_POST['email']; 
+}
+// else {
+//  $email = getEmail($_SESSION['username']);
+    $email = 'someemail@email.com';
+    echo $email;
+// }
 ?>
 </p>
-<form method="post" action="changeEmail.php">
+<form method="post" action="editProfile.php">
 New email: <input type="text" name="email">
-<input type="submit" value="Submit" name="submit">
+<input type="submit" value="Submit" name="emailsubmit">
 </form>
 
+<!-- Form to change a user's interests -->
 <p>Interests:</p><br>
 <?php
 //some php code to get current interests into an array
@@ -43,6 +92,7 @@ New email: <input type="text" name="email">
 //    echo '<input type="button" onclick="deleteInterest($interest)" value="Delete" />
 ?>
 
+<!-- Form to change a user's job description -->
 <p>Job Description:
 <?php
 $jobDescription = 'myjob';
@@ -55,6 +105,7 @@ New Job Description: <input type="text" name="jobdescription">
 <input type="submit" value="Submit" name="submit">
 </form>
 
+<!-- Form to change a user's skype ID -->
 <p>SkypeID: 
 <?php
 $skypeid = 'myskypeid';
@@ -79,11 +130,15 @@ use button after each <input type="button" onclick="deleteMentorOrMentee.php" va
 -->
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['passwordsubmit'])){
-    	echo 'password submitted';
-    }
-}
+
+/*
+ * Deletes an interest that is stored in a user profile
+ *
+ * @param array of strings $interest is an array that holds all of a user's interests as
+ *		strings
+ * 
+ * @return void
+ */
 function deleteInterest($interest) {
     echo 'interest deleted!';
     header('Refresh: 3; URL=http://localhost/editProfile.php');
--- viewProfiles.php
+++ viewProfiles.php
@@ -1,10 +1,14 @@
-<?php session_start(); ?>
+<?php session_start(); 
+include 'DatabaseFunctions.php';
+?>
 <html>
 <head>
 </head>
 <body>
 <h1>View Profiles</h1>
 <br>
+<!-- These forms allow a user to search for another user based on their username,
+email, or interests -->
 <form method="post" action="viewProfiles.php">
 Search by email: <input type="text" name="email">
 <input type="submit" value="Submit" name="emailsearchsubmit">
@@ -18,7 +22,6 @@ Search by username: <input type="text" name="username">
 <form method="post" action="viewProfiles.php">
 Search by a interest: <input type="text" name="interest">
 <input type="submit" value="Submit" name="interestsearchsubmit">
-<?php displaySkypeButton("mmilanovich");?>
 </form>
 <br>
 
@@ -36,34 +39,91 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+/*
+ * Searches for the specified username, if the username is valid,
+ * displays their information via echoing html code via calling displayUserProfile()
+ *
+ * @param String $username is the username that will be searched for
+ *
+ * @return void
+ */
 function displayUserProfileByUsername($username) {
 	//if username exists
-	//	echo user information
+	//	displayUserProfile($username);
 	//else
 		echo 'User not found, please enter a valid username';
 }
 
+/*
+ * Searches for user based on their email, if a matching username is found, 
+ * displays their information via echoing html code via calling displayUserProfile()
+ *
+ * @param String $email is the email that will be searched for in the database
+ *
+ * @return void
+ */
 function displayUserProfileByEmail($email) {
 	//if user with $email exists
-	//	echo user information
+	//	 displayUserProfile($user)
 	//else
 		echo 'User not found, please enter a valid email';
 }
 
+/*
+ * Searches for user based on their interests, if a matching username is found, 
+ * displays their information by echoing html code via calling displayUserProfile()
+ *
+ * @param String $interest is the interest that will be searched for in the database
+ *
+ * @return void
+ */
 function displayUserProfilesByInterest($interest) {
 	// if database returns items, display in a list
 	// else 
 	echo 'No users found with an interest in '.$interest.', please try another interest';
 }
 
-function displaySkypeButton($user) {
+/*
+ * Gets all user information from the database, and displays their information 
+ * via echoing html code 
+ *
+ * @param String $user is the user whos information will be displayed
+ *
+ * @return void
+ */
+function displayUserProfile($user) {
+	echo $user;
+	echo '<br>';
+	echo '<p>'.getEmail($user).'</p>';
+	echo '<br>';
+	displaySkypeButton(getskypeID($user));
+	echo '<br>';
+	$interestsArray = getInterests($user);
+	for ($i = 0; $i < count($interestsArray); $i++) {
+		echo '<p>'.$interestsArray[$i].'</p>';
+		echo '<br>';
+	}
+	echo '<p>'.getjobDescription($user).'</p>';
+}
+
+/*
+ * Takes a skype username and displays a functional skype calling button that when pressed
+ * will give the viewing user the option to initiate a call or initiate a text conversation
+ * with the user whos profile they are viewing.
+ *
+ * @param String $skypeID is the user's skype name that will be connected to the skype call
+ * button
+ *
+ * @return void
+ */
+function displaySkypeButton($skypeID) {
 echo '<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>';
 echo '<div id="SkypeButton_Call">';
 echo '<script type="text/javascript">';
 echo 'Skype.ui({';
 echo '"name": "dropdown",';
 echo '"element": "SkypeButton_Call",';
-echo '"participants": ["'.$user.'"],';
+echo '"participants": ["'.$skypeID.'"],';
 echo '"imageSize": 32';
 echo '});';
 echo '</script>';
