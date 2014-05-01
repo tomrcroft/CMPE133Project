08427d5311b966385f5cd8eebdd2e6efcebb5e27
jnguyen51596 <icare202@gmail.com>
2014-05-01 13:28:02 -0700

    Update and rename main_login.php to mainLogin.php

--- mainLogin.php
+++ mainLogin.php
@@ -7,37 +7,19 @@
  *
  */
 
-require "config.php";
-$tbl_name="members"; // Table name 
+include "DatabaseFunctions.php";
 
 //check to see if the user is connected
- if(isset($_SESSION['username'])){ 
-header("location:login_success.php");
- } 
+if(isset($_SESSION['username'])){ 
+header("location:mainWebpage.php");
+} 
 
 if(isset($_POST['submit'])) 
- { 
-// username and password sent from form 
-$myusername=$_POST['myusername']; 
-$mypassword=$_POST['mypassword']; 
-
-// To protect MySQL injection 
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
-
-// Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
-
-// If result matched $myusername and $mypassword, table row must be 1 row
-if($count==1){
+{ 
+if(validatePassword($_POST['myusername'],$_POST['mypassword'])== true){
 
 // Register $myusername, $mypassword and redirect to file "login_success.php"
-
-$_SESSION['myusername']= $myusername;
+$_SESSION['username']= $myusername;
 header("location:login_success.php");
 }
 else {
