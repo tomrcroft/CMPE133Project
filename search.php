7a69e59be204aba27c052183582ac44ed41163da
Jordan Watts <jdub9108@yahoo.com>
2014-05-02 15:50:58 -0700

    Search update with PHP functions

--- search.php
+++ search.php
@@ -14,12 +14,32 @@
  <body>
     <form action="action.php" method="post">
         <p>Are you searching for a Mentor or Mentee?<br>
-            <input type="radio" name="fav_color" value="Mentor" checked>Mentor<br>
-            <input type="radio" name="fav_color" value="Mentee">Mentee</p>
-        <p>General Subject: <input type="text" name="job description" /></p>
-        <p>Specific Category: <input type="text" name="interests" /></p>
+            <input type="radio" name="search_type" value="Mentor" checked>Mentor<br>
+            <input type="radio" name="search_type" value="Mentee">Mentee</p>
+        <p>Job Category: <input type="text" name="jobDescription" /></p>
+        <p>Specific Interests: <input type="text" name="interests" /></p>
         <p><input type="submit" name="Submit"/></p>
     </form> 
  </body>
 </html>
 
+<?php
+    $type = search_type;
+    $job = jobDescription;
+    $interests = interests;
+    
+    function getSearchType()
+    {
+        return $type;
+    }
+    
+    function getSeachJobDescription()
+    {
+        return $job;
+    }
+    
+    function getSearchInterests()
+    {
+        return $interests;
+    }
+?>
