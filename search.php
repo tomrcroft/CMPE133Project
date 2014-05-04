4f524dc1edbe430610a9af4729ac33c475982ccf
Jordan Watts <jdub9108@yahoo.com>
2014-05-04 00:50:23 -0700

    finally got the fucker almost done

--- search.php
+++ search.php
@@ -6,6 +6,21 @@
  * @author Jordan Watts
  * @copyright 2014
  */
+ 
+/*session_start(); 
+$loggedIn = isset($_SESSION['username']);
+if (!$loggedIn)
+	header('location:mainLogin.php');
+*/
+include 'DatabaseFunctions.php';
+
+if (isset($_POST['submit'])) { 
+ 	
+ if(register($_POST['job'] ,$_POST['interests'])==1)
+ {
+ 	//results = getUsernamesUsingInterests('job', 'interests');
+ }
+ }
 ?>
 
 <html>
@@ -14,33 +29,10 @@
  </head>
  <body>
     <form action="action.php" method="post">
-        <p>Are you searching for a Mentor or Mentee?<br>
-            <input type="radio" name="search_type" value="Mentor" checked>Mentor<br>
-            <input type="radio" name="search_type" value="Mentee">Mentee</p>
-        <p>Job Category: <input type="text" name="jobDescription" /></p>
+        <p>Job Category: <input type="text" name="job" /></p>
         <p>Specific Interests: <input type="text" name="interests" /></p>
-        <p><input type="submit" name="Submit"/></p>
+        <p><input type="submit" name="submit"/></p>
     </form> 
  </body>
 </html>
 
-<?php
-    $type = search_type;
-    $job = jobDescription;
-    $interests = interests;
-    
-    function getSearchType()
-    {
-        return $type;
-    }
-    
-    function getSeachJobDescription()
-    {
-        return $job;
-    }
-    
-    function getSearchInterests()
-    {
-        return $interests;
-    }
-?>
